921b0a750855860fc2a02a0515cd4168d82aca35
Charles Young <542charlieyoung@gmail.com>
2016-10-03 17:19:33 -0500

    working chat

--- popup.js
+++ popup.js
@@ -1,9 +1,13 @@
 var enter = document.getElementById("entered");
 enter.addEventListener("submit", chat, false);
 
-function chat() {
+function chat(enter) {
   var message = document.getElementById("message").value;
   var newMessage = document.getElementById("newMessage");
 
-  newMessage.firstChild.nodeValue = "test";
+  enter.which = enter.which || enter.keyCode;
+  if (enter.keyCode == 13) {
+    newMessage.innerHTML = message;
+  }
+
 }
