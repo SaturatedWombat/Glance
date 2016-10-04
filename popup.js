cb072d5c967b4e1478015ac75a0fe92a34e7170c
Charles Young <542charlieyoung@gmail.com>
2016-10-04 00:18:55 -0500

    note.js

--- popup.js
+++ popup.js
@@ -1,20 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
   var checkPageButton = document.getElementById('newtab');
   checkPageButton.addEventListener('click', function() {
+  window.open("newtab.html");
 
-
-    chrome.tabs.getSelected(null, function(tab) {
-      /*
-      d = document;
-
-      var f = d.createElement('form');
-      f.action = 'newtab.html';
-      f.method = 'post';
-      d.body.appendChild(f);
-      f.submit();
-      */
-      window.open("newtab.html");
-
-    });
   }, false);
 }, false);
