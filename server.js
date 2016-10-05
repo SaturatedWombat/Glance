ef6d3cbbbcf47056d7d6f3150be9d4b1cc8c1d93
Charles Young <542charlieyoung@gmail.com>
2016-10-04 19:35:40 -0500

    trying to fix aesthetics

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
     var lis = $("messages");
-    if(lis.length > 5) {
+    if(lis.length > 100) {
       lis.eq(lis.length - 1).remove();
     }
 
