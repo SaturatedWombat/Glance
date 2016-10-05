fdf0f059697e555156367180563b737497c32ebf
Charles Young <542charlieyoung@gmail.com>
2016-10-04 19:39:36 -0500

    trying to fix aesthetics

--- server.js
+++ server.js
@@ -9,13 +9,6 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
-    var lis = $("messages");
-    if(lis.length > 100) {
-      lis.eq(lis.length - 1).remove();
-      console.log("removed");
-    }
-
-    $("messages") = lis;
   });
   console.log('a user connected');
   socket.on('disconnect', function(){
