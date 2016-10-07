6562397e007df6f9d81e8292a14ffaa97b59ea50
Charles Young <542charlieyoung@gmail.com>
2016-10-06 23:08:26 -0500

    usernames

--- server.js
+++ server.js
@@ -2,21 +2,40 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var sanitize = require('validator').sanitize;
+var usernames = [];
+
+http.listen(process.env.PORT || 3000, function(){
+  console.log('listening on', http.address().port);
+});
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-});
 
-http.listen(process.env.PORT || 3000, function(){
-  console.log('listening on', http.address().port);
+  socket.on("user", function(name, callback) {
+    if (usernames.indexOf(name) !== -1) {
+      callback(false);
+    }
+    else {
+      callback(true);
+      socket.username = name;
+      usernames.push(socket.username);
+      io.emit("users", usernames);
+    }
+  });
+
+  socket.on('sent', function(msg){
+    io.emit('message', {msg: msg, name: socket.username});
+  });
+
+  socket.on('disconnect', function(name){
+    if(!socket.username) {
+      return;
+    }
+    usernames.splice(usernames.indexOf(socket.username, 1));
+    io.emit("users", usernames);
+  });
+
 });
