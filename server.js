1cee3483d120417e326d30897b6e39cfd97ed805
Charles Young <542charlieyoung@gmail.com>
2016-10-07 19:29:46 -0500

    private messages

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var sanitize = require('validator').sanitize;
-var usernames = [];
+var users = {};
 
 http.listen(process.env.PORT || 3000, function(){
   console.log('listening on', http.address().port);
@@ -14,28 +14,53 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
 
+  // username
   socket.on("user", function(name, callback) {
-    if (usernames.indexOf(name) !== -1) {
+    if (name in users || name === "") {
       callback(false);
     }
     else {
       callback(true);
       socket.username = name;
-      usernames.push(socket.username);
-      io.emit("users", usernames);
+      users[socket.username] = socket;
+      io.emit("users", Object.keys(users), socket.username, true);
     }
   });
 
-  socket.on('sent', function(msg){
-    io.emit('message', {msg: msg, name: socket.username});
+  // messaging
+  socket.on('sent', function(msg, callback){
+    var new_msg = msg.trim();
+    if(new_msg.substr(0,5) === "tell ") {
+      new_msg = new_msg.substr(5);
+      var ind = new_msg.indexOf(' ');
+      if (ind !== -1) {
+        var usern = new_msg.substr(0, ind);
+        new_msg = new_msg.substr(ind +1);
+        if (usern in users) {
+          users[usern].emit('priv', {msg: new_msg, name: ("from " + socket.username)});
+          users[socket.username].emit('priv', {msg: new_msg, name: ("to " + usern)});
+        }
+        else {
+          callback("Invalid user. Try again.");
+        }
+      }
+      else {
+        callback("No message received. Try again.");
+      }
+    }
+    else {
+      io.emit('message', {msg: new_msg, name: socket.username});
+    }
   });
 
+  // disconnect
   socket.on('disconnect', function(name){
     if(!socket.username) {
       return;
     }
-    usernames.splice(usernames.indexOf(socket.username, 1));
-    io.emit("users", usernames);
+    var temp = socket.username;
+    delete users[socket.username];
+    io.emit("users", Object.keys(users), temp, false);
   });
 
 });
